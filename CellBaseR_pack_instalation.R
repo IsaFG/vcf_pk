f139e4242710fc72d9b759ec8e566b8c84d52207
Isa <isa.folon@gmail.com>
2017-10-20 17:30:08 +0200

    (20-10) D+M : out-of-date files”

--- CellBaseR_pack_instalation.R
+++
@@ -1,8 +0,0 @@
-## CellBase R library
-## From https://github.com/opencb/cellbase/releases
-
-## Bioconductor link : https://bioconductor.org/packages/release/bioc/html/cellbaseR.html
-
-## try http:// if https:// URLs are not supported
-source("https://bioconductor.org/biocLite.R")
-biocLite("cellbaseR")
