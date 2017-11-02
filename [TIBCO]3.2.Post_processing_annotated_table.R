79980290ced79081591c6fe3328039fbdadb4442
Isa <isa.folon@gmail.com>
2017-11-02 11:21:00 +0100

    A+R : Improving script Post_processing_annotated_table. Edinting TIBCO files

---
+++ [TIBCO]3.2.Post_processing_annotated_table.R
@@ -0,0 +1,160 @@
+############## [INFO] Script 3.2 General information ############
+# This methods will process complex columns coming from a variants annotated table
+# , in order to give a readable table.
+# The user must choose the annotation he wishes to be displayed
+# and the script will extract this annotation and build a new table with the chosen annotation.
+
+############## [INFO] Input and Output ############################
+# Input 1: an annotated variant table, variable "annotVariants_table".
+# -> In TIBCO, this table is not readable and is stored as a "Document propertie"
+# Input 2: Avaiblable annotations, variable "availableAnnots"
+
+# NOTE: Both input tables come from the script : 3.cellbaseR_Query_getVariant
+
+# Output : expanded annotated variant tables, according to the annotation chosen by user.
+
+############# [INFO] Previous requirement ###############
+# This script will only work with specifics variables stored as R objects and with some libraries.
+# If you use RStudio, you need to run script 3.cellbaseR_Query_getVariant firstly
+# and keep in the R environment the variables described in input section above.
+
+############## [INFO] Problems ############################
+# I still have to test this scripts with other annotations.
+# Running this script throw the following error :
+# Warning messages:
+#   1: In data.frame(..., check.names = FALSE) :
+#   row names were found from a short variable and have been discarded
+
+# ############# [RStudio] Set Working directory ############
+# Please uncoment the next line changing the working directory by the correct one:
+# setwd("C:\\Users\\FollonIn\\Documents\\GitHub\\vcf_pk")
+
+############# [TIBCO] Load RinR library ###################
+library(RinR)
+
+########### [TIBCO] Determinate R interpreter location ########
+Rversion <- makeREvaluator("R", RHome = "C:/Program Files/R/R-3.4.1")
+
+############ [TIBCO] Load variables ###############
+# Load the annotated table, stored in memory as a Blob Object created by script "cellbaseR_Query_getVariant"
+annotVariantsSObject <- BlobToSObject(annotVariantsBlob)
+annotVariants_table <- annotVariantsSObject
+# Load annotation chosen by user
+# in this case the annotation "geneDrugInteraction" is pre-chosen for testing
+chosen_annot <- availableAnnots[8]
+
+########### [TIBCO] Create the REvaluate object ########
+AnnotatedTable <- REvaluate({
+  ############# [Code] Load libraries ############# 
+  library(RCurl)
+  library(jsonlite)
+  library(cellbaseR)
+  library(dplyr)
+  library(tidyr)
+
+  ############# [Code] Method to process cells containing a dataframe #############
+  # This function will convert any cell containing a dataframe in readable information
+  simplifyDFcell <- function (cell_to_convert) {
+    print ("Processing a dataframe") # testing line
+    
+    # Create a new dataframe from the cell
+    cell_df <- data.frame(cell_to_convert)
+    cell_dfBIS <- cell_df
+
+    # To know what classes are inside the dataframe
+    class_info <- lapply(cell_df, class)
+    classes_vec <- as.character(class_info)
+
+    i <- 1
+    for (class in classes_vec) {
+      if (class == "list") {
+        cell_df[,i] <- as.character(cell_df[,i])
+      # } else if (class == "data.frame") { # Esto no esta aun codificado, habra que ver si es necesario
+      #   # Remove the problematic column, split it and add it again to the dataframe
+      #   cell_df[,i] <- cbind(annotVariant[,-k], column[1], column[2])
+      }
+      i <- i + 1
+    }
+    
+    # The result will be a new row created from the cell
+    return(cell_df)
+  }
+  
+  ############# [Code] Main method ############
+  # Get the index of the chosen annotation
+  index_annot <- grep(chosen_annot, colnames(annotVariants_table))
+
+  # Extract, as a single variable, the chosen annotation from the table
+  annot_column <- annotVariants_table[,index_annot]
+  
+  # Declare the dataframe that will contain all the info of the chosen annotation
+  chosen_annot_table <- data.frame()
+  
+  # Determinate the class of all the cells inside the column
+  column_classes <- lapply(annot_column, class)
+
+  # For each cell of the annotation column, loop to extact the content of the cell
+  i = 1
+  for (annot_cell in annot_column) {
+    # Print info of the cell being analyzed
+    print (paste("Variant", i, "with class", class(annot_cell), "and lenght:",length(annot_cell)))
+    print (paste("dimension",(dim(annot_cell))))
+    
+    # Assign variables to start the analyze
+    data_class <- class(annot_cell)
+    data_length <- length(annot_cell)
+    basic_row <- basicTable[i,]
+    
+    # print (str(annot_column[i])) # testing line
+    
+    if (data_length == 0) { # In case the cell would have no result for this annotation
+      print ("Processing empty cell")
+      annotated_row <- basic_row
+      # annotated_row[, chosen_annot] <- "No result"
+      
+    } else if (data_class == "data.frame") { # In case the cell would contain a dataframe
+      # Apply the function  to produce a row/df from the cell
+      annotation_row <- simplifyDFcell(annot_cell)
+      
+      # Build a row with the chosen annotation of the current variant
+      # and bind it to basic table
+      annotated_row <- cbind(basic_row, annotation_row)
+      
+    } else if (data_class == "list") { # In case the cell would contain a list
+      annotation_row <- as.character(annot_cell)
+      
+      # Build a row with the chosen annotation of the current variant
+      # and bind it to basic table
+      annotated_row <- cbind(basic_row, annotation_row)
+      
+    } else {# In case the cell is not problematic
+      annotation_row <- annot_cell
+      
+      # Build a row with the chosen annotation of the current variant
+      # and bind it to basic table
+      annotated_row <- cbind(basic_row, annotation_row)
+    }
+
+    print ("Building the annotated table") # testing line
+    chosen_annot_table <- bind_rows(chosen_annot_table, annotated_row)
+    
+    i = i + 1
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")# testing line
+  }
+  chosen_annot_table # for TIBCO only
+},
+data = list(annotVariants_table = annotVariants_table, chosen_annot = chosen_annot, basicTable = basicTable)
+# ,
+# REvaluator = Rversion,
+# verbose	= TRUE
+)
+
+########### [TESTING] Control classes of the chosen annotation table #######
+# Just to be sure this table is suitable for TIBCO
+TEST_classes <- lapply(chosen_annot_table, class)
+View(annot_cell)
+
+########### [RStudio] Print the table in a txt file ###########
+# Works only with basic table
+file_path <- paste("test_files\\annotated_table_",chosen_annot, ".txt",sep = "")
+try(write.table(chosen_annot_table,file_path, append = FALSE, sep="\t",row.names=FALSE))
---
+++ _old/3.2.Post_processing_annotated_table.R
@@ -0,0 +1,99 @@
+############## [INFO] Script 3.2 General information ############
+# This methods will process complex columns coming from a variants annotated table.
+# The user must choose the annotation he wishes to be displayed
+# and the script will extract this annotation and build a new table with the chosen annotation.
+
+############## [INFO] Input and Output ############################
+# Input 1: an annotated variant table "annotVariants_table" 
+# Input 2: Avaiblable annotations, variable "availableAnnots"
+# NOTE: Both tables are R object created by the script : 3.cellbaseR_Query_getVariant
+
+# Output : expanded annotated variant tables, according to the annotation chosen by user.
+
+############# [INFO] Previous requirement ###############
+# This script will only work with specifics variables stored as R objects and with some libraries.
+# You need to run script 3.cellbaseR_Query_getVariant firstly
+# and keep in the R environment the variables described in input section above 
+
+############## [INFO] Problems ############################
+# I still have to test this scripts with other annotations.
+# Running this script throw the following error :
+# Warning messages:
+#   1: In data.frame(..., check.names = FALSE) :
+#   row names were found from a short variable and have been discarded
+
+# ############# [RStudio] Set Working directory ############
+# Please uncoment the next line changing the working directory by the correct one:
+# setwd("C:\\Users\\FollonIn\\Documents\\GitHub\\vcf_pk")
+
+# ############# [TIBCO] Load RinR library ###################
+# library(RinR)
+# 
+# ########### [TIBCO] Determinate R interpreter location ########
+# Rversion <- makeREvaluator("R", RHome = "C:/Program Files/R/R-3.4.1")
+
+############ [Code] Declare variables ###############
+# Declare a basic table that will have basic information about the variant
+basicTable <- annotVariants_table[,1:5]
+
+########### [RStudio] Let the user choose the annotation ############
+# This part must be done by TIBCO
+# In the next line, we have chosen the annotation "geneDrugInteraction" for testing
+chosen_annot <- availableAnnots[13]
+
+# Get the index of the chosen annotation
+index_annot <- grep(chosen_annot, colnames(annotVariants_table))
+
+############# [Code] Main method ############
+# Extract the chosen annotation from the table as a single column
+annot_column <- annotVariants_table[[paste(chosen_annot)]]
+
+# Declare the new dataframe
+chosen_annot_table <- data.frame()
+
+# Loop to extract the information from annotVariants_table
+i = 1
+for (annot_cell in annot_column) {
+  # Print info of the cell
+  print (paste("Variant", i, "with class", class(annot_cell), "and lenght:",length(annot_cell)))
+  print (paste("dimension",(dim(annot_cell))))
+  
+  # Assign variables
+  data_class <- class(annot_cell)
+  data_length <- length(annot_cell)
+  basic_row <- basicTable[i,]
+
+  # print (str(annot_column[i])) # testing line
+  
+  if (data_length == 0) {
+    print ("Processing empty cell")
+    annoted_row <- (basicTable[i,])
+    annoted_row[, chosen_annot] <- "No result"
+
+    } else if (data_class == "data.frame") {
+      print ("Processing dataframe") # testing line
+      
+      # Create a data.frame from the cell
+      cell_df <- data.frame(annot_column[i])
+      
+      # Build a row with the chosen annotation of the current variant and the basic table
+      annoted_row <- cbind(basicTable[i,], cell_df)
+
+    } else if (data_class == "list") {
+      print ("Processing list") # testing line
+      cell_df <- data.frame(annot_column[i])
+      annoted_row <- cbind(basicTable[i,], cell_df)
+  }
+  
+  print ("Building the annotated table") # testing line
+  chosen_annot_table <- bind_rows(chosen_annot_table, annoted_row)
+  
+  i = i + 1
+  print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")# testing line
+}
+
+########### [RStudio] Print the table in a txt file ###########
+# Works only with basic table
+file_path <- paste("test_files\\annotated_table_",chosen_annot, ".txt",sep = "")
+try(write.table(chosen_annot_table,file_path, append = FALSE, sep="\t",row.names=FALSE))
+
